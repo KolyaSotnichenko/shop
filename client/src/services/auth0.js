9b450b8e2b460485d0c736e9a86e9f365b8b9964
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2023-02-13 14:44:00 +0200

    change redirect url

--- client/src/services/auth0.js
+++ client/src/services/auth0.js
@@ -8,7 +8,7 @@ class Auth0Client {
       domain: 'dev-sdkioo3idqauf0m7.us.auth0.com',
       audience: 'https://dev-sdkioo3idqauf0m7.us.auth0.com/userinfo',
       clientID: 'nZDMgalW1UhK98Jnx1gumDYt9JmNFiDQ',
-      redirectUri: 'https://shop-8461.vercel.app/cabinet',
+      redirectUri: 'https://shop-azure-ten.vercel.app/cabinet',
       responseType: 'token id_token',
       scope: 'openid profile'
     });
